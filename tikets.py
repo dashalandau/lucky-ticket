256a3d7a7e2fcdcc7553e71002944b803f862331
our ame <onufrienkodarina@gmail.com>
2023-09-13 15:48:47 +0300

    add threading

--- tikets.py
+++ tikets.py
@@ -1,5 +1,6 @@
 import itertools
 import multiprocessing
+import threading
 
 
 def is_lucky(ticket):
@@ -8,41 +9,40 @@ def is_lucky(ticket):
     return sum(map(int, first_half)) == sum(map(int, second_half))
 
 
-def generate_tickets(length):
-    digits = '0123456789'
-    return [''.join(p) for p in itertools.product(digits, repeat=length)]
+def count_lucky_tickets(ticket_length, num_threads):
+    total_tickets = 10 ** ticket_length
+    tickets_per_thread = total_tickets // num_threads
 
+    def worker(start, end):
+        count = 0
+        for ticket in range(start, end):
+            ticket_str = str(ticket).zfill(ticket_length)
+            if is_lucky(ticket_str):
+                count += 1
+        return count
 
-def count_lucky_tickets(start, end, tickets):
-    count = 0
-    for i in range(start, end):
-        if is_lucky(tickets[i]):
-            count += 1
-    return count
-
-
-def main():
-    ticket_length = 6
-    num_processes = 4
-
-    tickets = generate_tickets(ticket_length)
-    chunk_size = len(tickets) // num_processes
-
-    pool = multiprocessing.Pool(processes=num_processes)
     results = []
+    threads = []
 
-    for i in range(num_processes):
-        start = i * chunk_size
-        end = (i + 1) * chunk_size if i < num_processes - 1 else len(tickets)
-        result = pool.apply_async(count_lucky_tickets, (start, end, tickets))
-        results.append(result)
+    for i in range(num_threads):
+        start = i * tickets_per_thread
+        end = (i + 1) * tickets_per_thread if i < num_threads - 1 else total_tickets
+        thread = threading.Thread(target=lambda: results.append(worker(start, end)))
+        threads.append(thread)
+        thread.start()
 
-    pool.close()
-    pool.join()
+    for thread in threads:
+        thread.join()
 
-    total_lucky_tickets = sum(result.get() for result in results)
-    print(f"Загальна кількість щасливих білетів: {total_lucky_tickets}")
+    return sum(results)
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    ticket_length = 6
+    num_threads = 4
+
+    if ticket_length % 2 != 0:
+        print("Довжина білета повинна бути парним числом (6, 8 або 10).")
+    else:
+        result = count_lucky_tickets(ticket_length, num_threads)
+        print(f"Кількість щасливих білетів з {ticket_length} цифрами: {result}")
